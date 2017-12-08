2a7e85e7eb47c633de64d9eaf1623bcb234e2507
tcl <123971479@qq.com>
2017-12-08 15:53:50 +0800

    need push

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(1,4,50)
+x = np.linspace(-6,4,50)
 
 y1 = 2*x + 1
 y2 = x**2
@@ -11,5 +11,8 @@ y2 = x**2
 plt.figure()
 plt.plot(x,y1)
 plt.plot(x,y2,color='red',linewidth=2.0,linestyle='--')
+
+plt.figure()
+plt.plot(x,y2)
 
 plt.show()
